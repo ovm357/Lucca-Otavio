d15227ad68f27aa6acef4aac6001d2feae6006f1
ovm357 <ovm357@gmail.com>
2015-03-06 10:06:57 -0300

    Adicionando inteligencia artificial (com erros)

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -21,8 +21,11 @@ public class Enquirer implements IEnquirer
 	{
         IBaseConhecimento bc = new BaseConhecimento();
 		String listaAnimais[] = bc.listaNomes();
-		int animal;
+		String storage[30];
+		int animal, contPer = 0, contFor;
+		boolean repetido;
 		
+		/* Este FOR garante que todos os animais sejam testados pelo Enquirer */
 		for (animal = 0; animal < listaAnimais.length; animal++) {
 			obj = bc.recuperaObjeto(listaAnimais[animal]);
 			IDeclaracao decl = obj.primeira();
@@ -30,15 +33,34 @@ public class Enquirer implements IEnquirer
 			System.out.println("!!!");
 	        boolean animalEsperado = true;
 			while (decl != null && animalEsperado) {
-				String pergunta = decl.getPropriedade();
-				String respostaEsperada = decl.getValor();
 				
-				String resposta = responder.ask(pergunta);
-				if (resposta.equalsIgnoreCase(respostaEsperada))
-					decl = obj.proxima();
-				else
-					animalEsperado = false;
+				String pergunta = decl.getPropriedade();
+				
+				/* Este FOR compara a questao atual com as guardadas no estoque. Se for igual, a flag "repetido" vira true e o loop acaba. Se nao for, adiciona-a na lista */
+				for(contFor = 0; contFor <= 30; contFor++) {
+					if (storage[contFor].equalsIgnoreCase(pergunta)) {
+						repetido = true;
+						break;
+					}
+					else storage[contPer] = pergunta;						
+				}
+				/* Conta as perguntas que ja passaram */
+				contPer++;
+				
+				/* Se a pergunta nao for repetida, ela pode ser feita */
+				if (!repetido) {
+					String respostaEsperada = decl.getValor();
+					
+					String resposta = responder.ask(pergunta);
+					if (resposta.equalsIgnoreCase(respostaEsperada))
+						decl = obj.proxima();
+					else
+						animalEsperado = false;
+				}
+				/* Reinicia "repetido" */
+				repetido = false;
 			}
+			/* Se sair do WHILE sem respostas negativas, o animal correto foi encontrado! O loop eh entao quebrado */
 			if (animalEsperado != false)
 				break;
 		}
@@ -49,10 +71,10 @@ public class Enquirer implements IEnquirer
 		System.out.println("hu3");
 		if (acertei)
 			System.out.println("Oba! Acertei!");
-		/*else{
+		else{
 			System.out.println("fuem! fuem! fuem!");
 			break;
-		}*/
+		}
 
 	}
 
