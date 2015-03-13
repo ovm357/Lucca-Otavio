646c1998fb7ae1f93bd9c6c034268d012006cb61
LuBollani <luc_bollani@hotmail.com>
2015-03-12 21:33:03 -0300

    Variaveis inutilizadas em responde, maze nao original !!

--- src2/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src2/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -1,7 +1,5 @@
 package pt.c02classes.s01knowledge.s02app.actors;
 
-import java.util.Scanner;
-
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 
@@ -14,27 +12,147 @@ public class EnquirerMaze implements IEnquirer {
 	}
 	
 	public boolean discover() {
-		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-		String tipo = scanner.nextLine();
-		while (!tipo.equalsIgnoreCase("F")) {
-		   System.out.print("  --> ");
-		   String pc = scanner.nextLine();
-		   switch (tipo.toUpperCase()) {
-		      case "P": String resposta = responder.ask(pc);
-		                System.out.println("  Resposta: " + resposta);
-		                break;
-		      case "M": boolean moveu = responder.move(pc);
-		                System.out.println((moveu)?"  Movimento executado!":"Não é possível mover");
-		                break;
-		   }
-			System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-			tipo = scanner.nextLine();
+		boolean saida = false;
+		
+	    String direcao = "temp";
+		
+	    /* Este bloco descobre qual a direcao inicial apos a entrada no labirinto */
+	    if (responder.ask("norte")==("passagem"))
+	    	direcao = "norte";
+	    else if (responder.ask("leste")==("passagem"))
+	    	direcao = "leste";
+	    else if (responder.ask("oeste")==("passagem"))
+	    	direcao = "oeste";
+	    else if (responder.ask("sul")==("passagem"))
+	    	direcao = "sul";
+	    
+		while(!saida) {
+		      switch(direcao) {
+			      
+			      /* NORTE */
+			      case("norte"):
+			    	  if (responder.ask("oeste")==("parede")) {
+			    		  if (!(responder.ask("norte")==("parede"))) {
+			    			  responder.move("norte");
+			    			  direcao = "norte";
+			    			  System.out.println(direcao);
+			    			  break;
+			    		  }
+			    		  else if (!(responder.ask("leste")==("parede"))) {
+			    			  responder.move("leste");
+				    		  direcao = "leste";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+				    	  else {
+			    			  responder.move("sul");
+				    		  direcao = "sul";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+			      }
+			      else {
+			    	  responder.move("oeste");
+			    	  direcao = "oeste";
+			    	  System.out.println(direcao);
+			    	  break;
+			      }
+			      
+			      /* LESTE */
+			      case("leste"):
+			    	  if (responder.ask("norte")==("parede")) {
+			    		  if (!(responder.ask("leste")==("parede"))) {
+			    			  responder.move("leste");
+			    			  direcao = "leste";
+			    			  System.out.println(direcao);
+			    			  break;
+			    		  }
+			    		  else if (!(responder.ask("sul")==("parede"))) {
+			    			  responder.move("sul");
+				    		  direcao = "sul";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+				    	  else {
+			    			  responder.move("oeste");
+				    		  direcao = "oeste";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+			      }
+			      else {
+			    	  responder.move("norte");
+			    	  direcao = "norte";
+			    	  System.out.println(direcao);
+			    	  break;
+			      }
+			      
+			      /* OESTE */
+			      case("oeste"):
+			    	  if (responder.ask("sul")==("parede")) {
+			    		  if (!(responder.ask("oeste")==("parede"))) {
+			    			  responder.move("oeste");
+			    			  direcao = "oeste";
+			    			  System.out.println(direcao);
+			    			  break;
+			    		  }
+			    		  else if (!(responder.ask("norte")==("parede"))) {
+			    			  responder.move("norte");
+				    		  direcao = "norte";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+				    	  else {
+			    			  responder.move("leste");
+				    		  direcao = "leste";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+			      }
+			      else {
+			    	  responder.move("sul");
+			    	  direcao = "sul";
+			    	  System.out.println(direcao);
+			    	  break;
+			      }
+			      
+			      /* SUL */
+			      case("sul"):
+			    	  if (responder.ask("leste")==("parede")) {
+			    		  if (!(responder.ask("sul")==("parede"))) {
+			    			  responder.move("sul");
+			    			  direcao = "sul";
+			    			  System.out.println(direcao);
+			    			  break;
+			    		  }
+			    		  else if (!(responder.ask("oeste")==("parede"))) {
+			    			  responder.move("oeste");
+				    		  direcao = "oeste";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+				    	  else {
+			    			  responder.move("norte");
+				    		  direcao = "norte";
+				    		  System.out.println(direcao);
+				    		  break;
+			    		  }
+			      }
+			      else {
+			    	  responder.move("leste");
+			    	  direcao = "leste";
+			    	  System.out.println(direcao);
+			    	  break;
+			      }
+		      }
+		      
+		      
+		      if (responder.ask("aqui")==("saida"))
+		    	  saida = true;
+		      
 		}
-		
-		scanner.close();
-		
+		System.out.println(responder.ask("aqui"));
 		return true;
 	}
 	
