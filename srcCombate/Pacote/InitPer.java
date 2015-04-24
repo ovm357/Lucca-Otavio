b2eba1c65735d76827d0a69047069ee9221ec9f6
ovm357 <ovm357@gmail.com>
2015-04-24 14:23:48 -0300

    COMBATE

---
+++ srcCombate/Pacote/InitPer.java
@@ -0,0 +1,15 @@
+package Pacote;
+
+import Pacote.Personagem;
+
+public class InitPer {
+	String time;
+	Personagem esse;
+	int iniciativa;
+
+	public InitPer (String team, Personagem actual, int init){
+		this.time = team;
+		this.esse = actual;
+		this.iniciativa = init;
+	}
+}
---
+++ srcCombate/Pacote/Personagem.java
@@ -0,0 +1,7 @@
+package Pacote;
+
+public class Personagem {
+int AGI;
+int LCK;
+}
+
